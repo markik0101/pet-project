ea6a2303d102c6f94bc82abdf5d517e50db7ea05
Mark <markik0101@gmail.com>
2022-08-04 22:19:57 +0300

    fetching data

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,12 +1,28 @@
-import React, {useState, useEffects} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 
 const PostsPage = () => {
-	
+	const [posts, setPosts] = useState([])
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/posts')
+			.then(response => response.json())
+			.then(data => setPosts(data))
+	}, [])
 
 	return (
 		<div>
 			<h1>Posts</h1>
+
+			<div className="posts">
+				{
+					posts.map((post) => (
+						<Link to="/" className="black">
+							<div key={post.id}><li>{post.title}</li></div>
+						</Link>
+					))
+				}
+			</div>
 		</div>
 	)
 }
